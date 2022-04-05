2f54a191b6ecad1d33fb8e6de61c3f1613375ca2
disor <disorn.bootso@gmail.com>
2022-04-05 17:01:42 +0700

    -Rearrange promotion service, controller

--- src/main/java/com/project/backend/models/Promotion.java
+++ src/main/java/com/project/backend/models/Promotion.java
@@ -1,5 +1,6 @@
 package com.project.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 @Table(name = "promotion")
 public class Promotion {
 
