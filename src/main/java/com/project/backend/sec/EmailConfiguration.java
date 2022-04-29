6fa84f8295009f785ae96383e16d59d69569b32b
Sardine <sardineop@gmail.com>
2022-04-30 00:23:02 +0700

    add class: EmailRequest,EmailConfiguration, and email-template.ftl for send html email

---
+++ src/main/java/com/project/backend/sec/EmailConfiguration.java
@@ -0,0 +1,18 @@
+package com.project.backend.sec;
+
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.context.annotation.Primary;
+        import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+
+@Configuration
+public class EmailConfiguration {
+
+    @Primary
+    @Bean
+    public FreeMarkerConfigurationFactoryBean factoryBean(){
+        FreeMarkerConfigurationFactoryBean bean = new FreeMarkerConfigurationFactoryBean();
+        bean.setTemplateLoaderPath("classpath:templates");
+        return bean;
+    }
+}
